567ecb16c1da9af68fb1c07fd7a9d403af950074
tiagodfigueiredo7 <tiagod.figueiredo@gmail.com>
2022-06-25 22:18:18 +0000

    teste

--- ec2.tf
+++ ec2.tf
@@ -1,16 +1,16 @@
-data "aws_ami" "ubuntu" {
-  most_recent = true
+data "aws_ami" "ubuntu" {           # essa linha pergunta pra Aws que queremos uma imagem! (aws_ami) qual queremos ? "ubuntu"
+  most_recent = true                # aqui estamos falando que queremos a mais recente -> true
 
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  filter {                          # esse filtro simples, baseado no NOME!
+    name   = "name"                 # queremos buscar o NOME
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]  # essa linha diz que queremos buscar lá uma AMI baseado no nome a mais recente
   }
 
-  owners = ["099720109477"] # Ubuntu
+  owners = ["099720109477"]         # aqui diz que é pra buscar a AMI baseado apenas nesse dono ["099720109477"]  = UBUNTU               
 }
 
 resource "aws_instance" "Terraform-TL" {
-  ami           = data.aws_ami.ubuntu.id
+  ami           = data.aws_ami.ubuntu.id     # essa linha é uma referencia a 1 linha
   instance_type = "t2.micro"
 
   tags = {
