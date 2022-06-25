7b8d48e52031a5b5f7e0a24fe2c3c9f6b6df2d7f
tiagodfigueiredo7 <tiagod.figueiredo@gmail.com>
2022-06-25 22:15:49 +0000

    teste

--- backend.tf
+++ backend.tf
@@ -1,7 +1,8 @@
 terraform {
-        backend "s3" {
-            bucket = "tiago-terraform-tfstate"
-            key = terraformlt
-            region = us-east-2
-        }
+  backend "s3" {
+    bucket = "tiago-terraform-tfstate"
+    key    = "awsterralt"
+    region = "us-east-2"
+  }
 }
+
