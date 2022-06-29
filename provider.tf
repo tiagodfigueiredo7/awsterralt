5141bc186de564bd997a06412f484f7e0be43e62
tiagodfigueiredo7 <tiagod.figueiredo@gmail.com>
2022-06-29 23:27:36 +0000

    teste

--- provider.tf
+++ provider.tf
@@ -1,3 +1,3 @@
 provider "aws" {
-  region  = count = "${terraform.workspace == "production" ? "us-east-2" : "us-east-1"}"
+  region  = count = "${terraform.workspace == "production" ? "us-east-2" : "us-east-1"}" # aqui é uma condição para que o Ec2 seja criando dependendo do workspace
 }
